52a75d99ddb9fcbe6571d928e756943c419c3219
skyjoe999 <74079962+skyjoe999@users.noreply.github.com>
2021-11-22 12:45:59 -0500

    Add files via upload

--- html_generators/intro_converter.py
+++ html_generators/intro_converter.py
@@ -1,124 +1,116 @@
+from bs4 import BeautifulSoup
 import requests
 import os
 import io
 from PIL import Image
-import re
+def templateFill(text,replace):
+	while(text.find('%')!=-1):
+		token=text[text.find('%')+1:text.find('%',text.find('%')+1)]
+		text=text[:text.find('%')]+'%'+text[text.find('%',text.find('%')+1)+1:]
+		if token not in replace:
+			print('error token:',token,text)
+			break
+		if type(replace[token])==type([]):
+			for i in replace[token]:
+				text=text[:text.find('%')]+templateFill(text[text.find('{')+1:text.find('}%'+token+'%')],replace|i)+text[text.find('%'):]
+			text=text[:text.find('%')]+text[text.find('}%'+token+'%')+len('}%'+token+'%'):]
+		elif type(replace[token])==type(True):
+			if replace[token]:
+				text=text[:text.find('%')]+templateFill(text[text.find('{')+1:text.find('}%'+token+'%')],replace)+text[text.find('%'):]
+			text=text[:text.find('%')]+text[text.find('}%'+token+'%')+len('}%'+token+'%'):]
+		elif type(replace[token])==type(''):
+			text=text[:text.find('%')]+replace[token]+text[text.find('%')+1:]
+		elif type(replace[token])==type({}):
+			text=text[:text.find('%')]+templateFill(text[text.find('{')+1:text.find('}%'+token+'%')],replace|replace[token])+text[text.find('}%'+token+'%')+len('}%'+token+'%'):]
+		else:
+			print('error type:',token,'is type',type(replace[token]))
+	return text
+
 chapter_count=45
 for c in range(1,chapter_count+1):
 	print('\tconverting chapter #'+str(c))
 	html=requests.get('https://www.macmillanhighered.com/BrainHoney/Resource/6716/digital_first_content/trunk/test/hillis2e/hillis2e_ch'+('0'+str(c))[-2:]+'_1.html').content.decode('utf-8')
+	soup=BeautifulSoup(html,'html.parser')
 
-	#links cleanup
-	while 1:
-		match=re.match(r'.*?(<span data-type="link" data-PNlinktype="ConceptLink" data-href="(hillis2e_ch(\d+)_(\d+)\.html)">(.*?)</span>)',html,16)
-		if match == None:
-			break
-		if match.group(4)=='1':
-			print('replaced a link to intro #'+match.group(3).lstrip('0'))
-			html=html[:match.start(1)]+'<a class="concept_link" target="_blank" href="../../ch'+match.group(3).lstrip('0')+'/intro/intro.html">'+match.group(5)+'</a>'+html[match.end(1):]
-		else:
-			print('replaced a link to section '+match.group(3).lstrip('0')+'.'+str(int(match.group(4))-1))
-			html=html[:match.start(1)]+'<a class="concept_link" target="_blank" href="https://www.macmillanhighered.com/BrainHoney/Resource/6716/digital_first_content/trunk/test/hillis2e/'+match.group(2)+'">'+match.group(5)+'</a>'+html[match.end(1):]
-	while 1:
-		match=re.match(r'.*?(<span data-type="termref" data-term=".*?">(.*?)</span>)',html,16)
-		if match == None:
-			break
-		print('replaced a "termref" defining '+match.group(2))
-		html=html[:match.start(1)]+'<a class="term_definition" target="_blank" href="https://www.google.com/search?q='+match.group(2)+'">'+match.group(2)+'</a>'+html[match.end(1):]
-	while 1:
-		match=re.match(r'.*?(<span data-type="link" data-PNlinktype="FigureLink".*?data-href="(asset/img_ch(\d+)/c13_fig(\d+).jpg)">(.*?)</span></span>)',html,16)
-		if match == None:
-			break
-		print('replaced a link to figure '+match.group(3).lstrip('0')+'.'+match.group(4).lstrip('0'))
-		html=html[:match.start(1)]+'<a class="figure_link" target="_blank" href="https://www.macmillanhighered.com/BrainHoney/Resource/6716/digital_first_content/trunk/test/hillis2e/'+match.group(2)+'">'+match.group(5)+'</a>'+html[match.end(1):]
-	while 1:
-		match=re.match(r'.*?(<span data-PNtype="nowrap">(.*?)</span>)',html,16)
-		if match == None:
-			break
-		print('replaced a "nowrap" span')
-		html=html[:match.start(1)]+'<span class="no_wrap">'+match.group(2)+'</span>'+html[match.end(1):]
-	while 1:
-		match=re.match(r'.*?(<div data-block_type="blockquote" (id=".*?")><p>(.*?)</p></div>)',html,16)
-		if match == None:
-			break
-		print('replaced a "blockquote" div')
-		html=html[:match.start(1)]+'<p '+match.group(2)+' class="blockquote">'+match.group(3)+'</p>'+html[match.end(1):]
+	for i in soup.find_all(attrs={'data-pnlinktype':'ConceptLink'}):
+		print('replaceing concept link:',i.get('data-href'),i.text)
+		n=soup.new_tag('a',attrs={'class':'concept_link','target':'_blank'},href=
+			('../../ch'+i.get('data-href')[i.get('data-href').rfind('ch')+2:i.get('data-href').rfind('_')].strip('0')+'/intro/intro.html'
+				if i.get('data-href')[i.get('data-href').rfind('_')+1:i.get('data-href').rfind('.')]=='1' else
+			'https://www.macmillanhighered.com/BrainHoney/Resource/6716/digital_first_content/trunk/test/hillis2e/'+i.get('data-href'))
+		)
+		n.string=i.text
+		i.replace_with(n)
+	for i in soup.find_all(attrs={'data-type':'termref'}):
+		print('replaceing termref:',i.get('data-term'),i.text)
+		n=soup.new_tag('a',attrs={'class':'term_definition','target':'_blank'},href='https://www.google.com/search?q='+i.text)
+		n.string=i.text
+		i.replace_with(n)
+	for i in soup.find_all(attrs={'data-pnlinktype':'FigureLink'}):
+		print('replaceing figure link:',i.get('data-href'),i.text)
+		n=soup.new_tag('a',attrs={'class':'figure_link','target':'_blank'},href=
+			'https://www.macmillanhighered.com/BrainHoney/Resource/6716/digital_first_content/trunk/test/hillis2e/'+i.get('data-href'))
+		n.string=i.text
+		i.replace_with(n)
+	for i in soup.find_all(attrs={'data-block_type':'blockquote'}):
+		print('replaceing blockquote')
+		i.p.attrs={'class':'blockquote','id':i.get('id')}
+		i.replace_with(i.p)
+	for i in soup.find_all(attrs={'data-pntype':'nowrap'}):
+		print('replacing nowrap:',i.text)
+		i.attrs={'class':'no_wrap'}
 
-	#item parsing
-	title=re.match(r'.*?<h2 class="section-title"><span data-type="title" data-title-for="hillis2e-ch\d+-section-1">(.*?)</span></h2>',html,16).group(1)
-	sections=re.match(r'.*?<h3 data-type="title" data-for_type="box" data-title-for="hillis2e-ch\d+-box-1">Key Concepts</h3>(.*?)</div>',html,16).group(1)
-	sections=list(map((lambda a: (a.group(1),a.group(2),str(int(a.group(1))+1))),list(map((lambda a:re.match(r'<p id="hillis2e-ch\d+-p-\d+"><strong>\d+\.(.*?)</strong>(.*?)</p>',a,16)),sections.split('\n')[1:-1]))))
-	img=(lambda a: (a.group(1),a.group(2)))(re.match(r'.*?<img id=".*?".*?src="(asset/img_ch\d+/(.*?))"',html,16))
-	imgCap=re.match(r'.*?<div data-type="figure_text"><span data-type="caption">(.*?)</span></div>',html,16).group(1)
-	paragraphs=re.match(r'.*?Key Concepts</h3>.*?</div>.*?(?=<p)(.*?)<div',html,16).group(1)
-	paragraphs=list(map((lambda a:(lambda a: [a.group(3),a.group(2),int(a.group(1))])(re.match(r'<p id="hillis2e-ch\d+-p-(\d+)"(.*?)>(.*?)</p>',a,16))),paragraphs.strip().split('\n')))
-	paragraphs[0][0]='<span class="first_letter">'+paragraphs[0][0][0]+'</span>'+paragraphs[0][0][1:]
-	paragraphs.sort(key=(lambda a:a[2]))
-	question=[False]
-	if re.match(r'.*?<div data-type="box"\ data-block_type="question"\ id="hillis2e-ch\d+-box-\d+">',html,16)!=None:
-		question=[True,re.match(r'.*?<h3>Question.*?<p id="hillis2e-ch\d+-p-\d+">(.*?)</p>',html,16).group(1),sections[-1][0],str(int(sections[-1][0])+1)]
 
-	#loading template
+	intro_info={
+		'c': str(c),
+		'c0': ('0'+str(c))[-2:],
+		'chapter_title': soup.find(class_='section-title').span.text,
+		'key_concepts': [
+			{
+				'concept_id': a.get('id').split('-')[-1],
+				'concept_number': a.strong.text,
+				'concept_title': a.text[a.text.find(' ')+1:]
+			} for a in soup.find(attrs={'data-block_type':'concepts'}).find_all('p')
+		],
+		'img_src': soup.find(attrs={'data-block_type':'fig_chapter_opener'}).img.get('src'),
+		'img_title': soup.find(attrs={'data-block_type':'fig_chapter_opener'}).img.get('src').split('/')[-1],
+		'img_caption': soup.find(attrs={'data-type':'figure_text'}).span.decode_contents(),
+		'paragraphs': [
+			{
+				'paragraph_text': ('<span class="first_letter">'+a.decode_contents()[0]+'</span>'+a.decode_contents()[1:]) if n==0 else a.decode_contents(),
+				'paragraph_class': ' class="'+a.get('class')+'"' if a.get('class')!=None else '',
+				'id': a.get('id').split('-')[-1]
+			} for n,a in enumerate(soup.div.div.find_all('p',recursive=False))
+		],
+		'do_question': soup.find(attrs={'data-block_type':'question'})!=None
+	}
+	intro_info['question']={
+		'question_text': soup.find(attrs={'data-block_type':'question'}).div.div.p.decode_contents(),
+		'question_concept_number': intro_info['key_concepts'][-1]['concept_number'],
+		'question_concept_id': intro_info['key_concepts'][-1]['concept_id']
+	} if intro_info['do_question'] else {}
+	intro_info['paragraphs'].sort(key = (lambda a:int(a['id'])))
+
 	file=open('./intro.template','r',encoding='utf-8')
 	template=file.read()
 	file.close()
-	def templateFill(text,replace):
-		while(text.find('%')!=-1):
-			token=text[text.find('%')+1:text.find('%',text.find('%')+1)]
-			text=text[:text.find('%')]+'%'+text[text.find('%',text.find('%')+1)+1:]
-			if token.startswith('if '):
-				token=token[3:]
-				if token not in replace:
-					print('error if token:',token)
-					break
-				if replace[token][0]:
-					tempRep=replace.copy()
-					for n,v in enumerate(replace[token][1:]):
-						tempRep[str(n)]=v
-					text=text[:text.find('%')]+templateFill(text[text.find('{')+1:text.find('}')],tempRep)+text[text.find('}')+1:]
-				else:
-					text=text[:text.find('%')]+text[text.find('}')+1:]
-				continue
-			if token not in replace:
-				print('error token:',token)
-				break
-			if type(replace[token])==type([]):
-				for i in replace[token]:
-					tempRep=replace.copy()
-					for n,v in enumerate(i):
-						tempRep[str(n)]=v
-					text=text[:text.find('%')]+templateFill(text[text.find('{')+1:text.find('}')],tempRep)+text[text.find('%'):]
-				text=text[:text.find('%')]+text[text.find('}')+1:]
-				continue
-			text=text[:text.find('%')]+replace[token]+text[text.find('%')+1:]
-		return text
 
-	#filling template and writing file
 	os.makedirs(os.path.dirname(os.path.dirname(__file__)).replace('\\','/')+'/chapters/ch'+str(c)+'/intro', exist_ok=True)
 	file=open(os.path.dirname(os.path.dirname(__file__)).replace('\\','/')+'/chapters/ch'+str(c)+'/intro/intro.html','w',encoding='utf-8')
 	file.write(
 		templateFill(template,
-			replace={
-				'c':str(c),
-				'c0':('0'+str(c))[-2:],
-				'title':title,
-				'imgCap':imgCap,
-				'img1': img[1],
-				'sections':sections,
-				'paragraphs':paragraphs,
-				'question': question
-			}
+			replace=intro_info
 		)
 	)
 	file.close()
-
-	#getting the intro image
-	ir=requests.get('https://www.macmillanhighered.com/BrainHoney/Resource/6716/digital_first_content/trunk/test/hillis2e/'+img[0], stream = True)
-	ir.raw.decode_content = True
-	im=Image.open(io.BytesIO(ir.content))
-	im.save(os.path.dirname(os.path.dirname(__file__)).replace('\\','/')+'/chapters/ch'+str(c)+'/intro/'+img[1])
-	im.close()
-'''
+	# #getting the intro image
+	# ir=requests.get('https://www.macmillanhighered.com/BrainHoney/Resource/6716/digital_first_content/trunk/test/hillis2e/'+img[0], stream = True)
+	# ir.raw.decode_content = True
+	# im=Image.open(io.BytesIO(ir.content))
+	# im.save(os.path.dirname(os.path.dirname(__file__)).replace('\\','/')+'/chapters/ch'+str(c)+'/intro/'+img[1])
+	# im.close()
 '''
 2✔ 6✔ 10✔ 13✔ 16✔ 32✔ 36✔ 38 39✔
+16:13.6
+22:22.18
 '''
